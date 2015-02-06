e05069db31dc394c2c2214447dcafcbebd8835fd
zpalmer <zoepalmer2014@u.northwestern.edu>
2015-02-05 20:48:32 -0600

    notes on git commands

--- helloworld.rb
+++ helloworld.rb
@@ -1,4 +1,18 @@
 # Hello World in Ruby -- Install day 12/17/12
 
+# Process for using Git
+#git add helloworld.rb
+# This adds a file to be committed
+#git commit -m "whatever you want to say"
+# This marks as, hey, here's the next thing I want to commit: bookmarked, sort of
+#git push origin master
+# And this one actually uploads (all commits since last push)
+
+#git checkout -b nameofbranch
+# creates a new branch
+# git status
+# tells you if you've changed stuff
+
+
 puts "hello world"
 puts "hello ruby!"
